e02ae6d7513d877c22fa701d2b397a234b2ee974
Harsh Patle <harshpatle491@gmail.com>
2025-09-11 00:46:48 +0530

    some changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar({ onShowAddModal }) {
     <Card.Header className="bg-light text-dark d-flex flex-column flex-md-row justify-content-between align-items-center">
       <h2 className="mb-2 mb-md-0"><i className="fa-solid fa-users-gear me-3"></i>Employee Management System</h2>
       <Button className="py-2 w-50 w-md-100" variant="outline-primary" onClick={onShowAddModal}>
-        <i className="fas fa-user-plus me-2"></i> Add Employee
+        <i className="fas fa-user-plus me-2"></i> Add cia Employee
       </Button>
     </Card.Header>
   );
